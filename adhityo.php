f431b336ff287a51ee3466b82b1926a80d344f77
Adhityo16 <adhityo23ti@mahasiswa.pcr.ac.id>
2024-11-19 23:34:20 +0700

    mengunggah file

---
+++ adhityo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>file tyo</title>
+</head>
+<body>
+   <h1>ini file tyo</h1> 
+   <h1>jangan di ganggu</h1> 
+   <h1></h1> 
+</body>
+</html>
